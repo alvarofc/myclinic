efc1c50be630c7753497e1e2d3c7d504712a89f6
Alvaro Fragoso <alvaro.fragoso@basf.com>
2023-07-26 13:55:35 +0200

    added new user modal

---
+++ myclinic/src/components/NewUser.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import { Button, Modal,Input, Label, Helper,Textarea, Fileupload  } from 'flowbite-svelte'
+    export let clickOutsideModal:boolean = false;
+  </script>
+  
+  
+  <Modal title="New patient" bind:open={clickOutsideModal} autoclose outsideclose>
+    <form action="">
+        <div class="grid grid-cols-2 sm:grid-cols-1 gap-4">
+        <div>
+        <Label for="name" class="pl-1">Name</Label>
+        <Input id="name" placeholder="Enter your name" required/>
+        <Helper>Enter your full name</Helper>
+    </div>
+    <div>
+        <Label for="surname" class="pl-1">Surname</Label>
+        <Input id="surname" placeholder="Enter surname" required/>
+        <Helper>Enter your full name</Helper>
+    </div>
+    <div>
+        <Label for="email" class="pl-1">Email</Label>
+        <Input id="email" placeholder="example@email.com" required/>
+        <Helper>Enter your email address</Helper>
+    </div>
+    <div>
+        <Label for ="phone" class="pl-1">Phone</Label>
+        <Input id="phone" placeholder="Enter your phone number" required/>
+        <Helper>Enter your phone number</Helper>
+    </div>
+    
+    <div>
+        <Label for="anotation" class="pl-1">Anotation</Label>
+        <Textarea id="anotation" placeholder="Enter your anotation" />
+        <Helper>Enter your anotation</Helper>
+    </div>
+</div>
+        
+    </form>
+    <svelte:fragment slot='footer'>
+      <Button on:click={() => alert('Handle "success"')} color="blue">Add patient</Button>
+      <Button color="alternative">Cancel</Button>
+    </svelte:fragment>
+  </Modal>
--- myclinic/src/routes/dashboard/patients/+page.svelte
+++ myclinic/src/routes/dashboard/patients/+page.svelte
@@ -1,10 +1,15 @@
+<script lang=ts>
+	import NewUser from "../../../components/NewUser.svelte";
+    let userModal:boolean = false;
+</script>
 
+<NewUser bind:clickOutsideModal={userModal}/>
 <main class="p-4 md:ml-64 h-auto pt-20">
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
     <div class="flex items-center justify-between pb-4 bg-white dark:bg-gray-900">
         <div>
-            <button  class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
-                <span class="sr-only">Action button</span>
+            <button  on:click={()=>userModal = true} class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
+                
                 New patient
                 <svg class="w-[18px] h-[18px] text-gray-800 dark:text-white pl-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
